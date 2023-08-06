0049b050a126499553a5e1ea11774fc0fff0fb54
yangming <843670891@qq.com>
2023-08-06 11:14:54 +0800

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+// import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+const container = document.getElementById("root");
+if (container) {
+  const root = ReactDOM.createRoot(container);
+  root.render(
+    <App />
+  );
+} else {
+  throw new Error("The root container not found !")
+}
+
